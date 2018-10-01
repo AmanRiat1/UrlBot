c2d2b1a93bb9dd48b8b4ad1a4805a0557c4d3f61
Aman Riat <amanriat777@gmail.com>
2018-09-30 21:30:24 -0400

    Added link through hard

--- URlTest.py
+++ URlTest.py
@@ -1,4 +1,3 @@
-# Work with Python 3.6
 import discord
 
 TOKEN = 'NDk1NDMwNTMzNTY4NzkwNTI4.DpE_Yg.vFSZLtuhm3wTYd0FSdOhlzdqkAY'
@@ -7,12 +6,11 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith('!link'):
+        msg = " Here's the link {0.author.mention} https://www.google.ca/".format(message)
         await client.send_message(message.channel, msg)
 
 client.run(TOKEN)
