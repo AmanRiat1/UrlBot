407e795e8ed67d90fcefaff58a55dae736ec9511
Aman Riat <amanriat777@gmail.com>
2018-12-22 17:09:48 -0500

    Added functions

--- URlTest.py
+++ URlTest.py
@@ -1,20 +1,41 @@
 import discord
-
-TOKEN = 'NDk1NDMwNTMzNTY4NzkwNTI4.DuRBKA.f34cK_LnmCNK6c62O-6n8uNjlJk'
+from discord.ext import commands
+TOKEN = 'NDk1NDMwNTMzNTY4NzkwNTI4.DuS9ZQ.tZcHzt8kMr7CJvBmb0jB7eT78Hs'
 
 client = discord.Client()
 
+bot = commands.Bot(command_prefix='!')
+
 @client.event
 async def on_message(message):
-    a = 'https://www.google.ca'
+    link = 'https://www.google.ca'
+    strike = 0
+    
     if message.author == client.user:
         return
-
+    
+    if message.content.startswith('!editme'):
+        new_link = message.content[8:]
+        link = new_link
+        await client.send_message(message.channel, new_link)
+    
     if message.content.startswith('!link'):
-        msg = " Here's the link {}".format(a)
+        msg = " Here's the link {}".format(link)
         await client.send_message(message.channel, msg)
 
+    if message.content.startswith('!strike'):
+        strike += 1
+        msg = '{} strikes'.format(strike)
+        await client.send_message(message.channel,msg)
 
-        
+    if message.content.startswith('!kick'):
+        kick(discord.Member)
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+    
+
 
 client.run(TOKEN)
